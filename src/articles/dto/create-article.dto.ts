0c649f9b53b0c33481eb811436fb33fa6611a1ea
Preston Kwan <sfkwan@yahoo.com>
2023-12-29 15:53:12 +0800

    enable validation

--- src/articles/dto/create-article.dto.ts
+++ src/articles/dto/create-article.dto.ts
@@ -1,15 +1,33 @@
 import { ApiProperty } from '@nestjs/swagger';
-
+import {
+  IsBoolean,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 export class CreateArticleDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(5)
   @ApiProperty()
   title: string;
 
+  @IsString()
+  @IsOptional()
+  @IsNotEmpty()
+  @MaxLength(300)
   @ApiProperty({ required: false })
   description: string;
 
+  @IsNotEmpty()
+  @IsString()
   @ApiProperty()
   body: string;
 
+  @IsBoolean()
+  @IsOptional()
   @ApiProperty({ required: false, default: false })
   published: boolean;
 }
